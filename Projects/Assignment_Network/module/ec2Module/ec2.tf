fadc8fb2fbf696ecd4cd6b7f4eb9029d3cf2cdc2
Sarmistha_Jena <sarmistha_jena@epam.com>
2024-01-22 21:02:22 +0530

    Added assignment 4 - VPC-fix

--- Projects/Assignment_Network/module/ec2Module/ec2.tf
+++ Projects/Assignment_Network/module/ec2Module/ec2.tf
@@ -23,16 +23,21 @@ resource "random_shuffle" "subnets" {
   result_count = 1
 }
 
-resource "aws_instance" "database" {
+resource "aws_instance" "server" {
   ami                    = module.amiModule.ami
   instance_type          = var.type
   key_name               = var.keyName
-  vpc_security_group_ids = [module.sg.sgPrivate]
-  subnet_id              = random_shuffle.subnets.result[0]
-  availability_zone      = data.aws_availability_zones.example.names[1]
+  vpc_security_group_ids = [module.sg.sgPub]
+  subnet_id              = module.subnet.publicSubnetId
+  availability_zone      = data.aws_availability_zones.example.names[0]
 
   associate_public_ip_address = true
+  user_data                   = <<-EOF
+#!/bin/bash
+echo '<html><body><h1 style="font-size:50px;color:blue;">Hello SARA</h1></body></html>
+nohup busybox httpd -f -p 8080 &
+EOF
   tags                        = {
-    Name = "TF DB server"
+    Name = "TF Web server"
   }
 }
