9be9a2c5290575e21c58ed8c27b11168f95c38e2
Sarmistha_Jena <sarmistha_jena@epam.com>
2024-01-22 20:12:56 +0530

    Added assignment 4 - VPC-fix

--- Projects/Assignment_Network/main.tf
+++ Projects/Assignment_Network/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hasicorp/aws"
+      version = "~> 1.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
 module "vpc_complete" {
   source = "./module/ec2Module"
 }
--- Projects/Assignment_Network/module/vpcModule/vpc.tf
+++ Projects/Assignment_Network/module/vpcModule/vpc.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hasicorp/aws"
-      version = "~> 1.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
