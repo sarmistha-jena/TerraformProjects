3b12b3ba700efaec497b3f855270ca8195e0269a
Sarmistha_Jena <sarmistha_jena@epam.com>
2024-01-23 16:43:41 +0530

    Added assignment 4 - VPC-fix

--- Projects/Assignment_Network/main.tf
+++ Projects/Assignment_Network/main.tf
@@ -2,14 +2,30 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+module "vpc" {
+  source = "./module/vpcModule"
+}
+
+module "subnets" {
+  source = "./module/subnetModule"
+  depends_on = [module.vpc]
+}
+
+module "igw" {
+  source = "./module/internetGatewayModule"
+  depends_on = [module.vpc]
+}
+/*
+
+*/
 /*module "subnet" {
   source = "./module/subnetModule"
-}*/
+}*//*
+
 module "vpc_complete-web" {
   source = "./module/ec2Module"
   //subnets = keys(module.subnet.publicSubnetId)
 }
-/*
 module "vpc_complete-db" {
   source = "./module/dbEc2Module"
   //subnets = keys(module.subnet.privateSubnetId)
