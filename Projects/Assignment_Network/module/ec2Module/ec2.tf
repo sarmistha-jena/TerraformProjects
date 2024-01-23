61aa64d7e35d354993a31bed9832d2a58af7da35
Sarmistha_Jena <sarmistha_jena@epam.com>
2024-01-23 18:37:27 +0530

    Added assignment 4 - VPC-fix

--- Projects/Assignment_Network/module/ec2Module/ec2.tf
+++ Projects/Assignment_Network/module/ec2Module/ec2.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "server" {
   associate_public_ip_address = true
   user_data                   = <<-EOF
 #!/bin/bash
-echo '<html><body><h1 style="font-size:50px;color:blue;">Hello SARA</h1></body></html>
+echo '<html><body><h1 style="font-size:50px;color:blue;">Sarmistha <br> <font style="color:red;"> www.myweb.com <br> <font style="color:green;"> Learn Devops </h1> </body></html>' > index.html
 nohup busybox httpd -f -p 8080 &
 EOF
   tags                        = {
