bf051f1b63f1e0fbc41fa441c8d53740cd92c358
Sarmistha_Jena <sarmistha_jena@epam.com>
2024-01-28 18:58:12 +0530

    Added assignment 5 - ELB-fix6

--- Projects/Assignment_ELB/module/elbModule/elbMain.tf
+++ Projects/Assignment_ELB/module/elbModule/elbMain.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "all" {}
 resource "aws_elb" "loadBalancer" {
   name               = "sara-elb-demo"
   security_groups    = [var.sgElb]
-  availability_zones = data.aws_availability_zones.all.names
+  //availability_zones = data.aws_availability_zones.all.names
   subnets = var.subnetId.*.id
 
   health_check {
